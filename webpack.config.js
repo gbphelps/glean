e72319259ef25ce52ac14f100cddbf991d391eb1
Grant Phelps <gbphelps2@gmail.com>
2020-08-14 19:07:05 -0400

    media queries

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
-      })
-    ],
-    module: {
-        rules: [ {
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader'
-            },
-          },  {
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader',]
-          }]
-    }
-}
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.(ts|js)x?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    }, {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    }, {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    }],
+  },
+};
